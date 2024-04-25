77fc095fcfe2404ab1ef0b7854ac4fe7eb4c9348
ahakuoku <ahakuoku@gmail.com>
2024-04-25 21:07:12 +0900

    追加：動作に必要な関数

--- src/main.py
+++ src/main.py
@@ -3,21 +3,43 @@
 import subprocess
 import psutil
 import config
+import time
+import discord
+import re
 
 # 変数定義
 server_path = config.server_folder_path + '/' + config.server_name
 server_save = '' + config.port_number
+client = discord.Client()
 
 # 関数定義
+def get_nettool_pw():
+    simuconf_path = config.server_folder_path + '/config/simuconf.tab'
+    f = open(simuconf_path, 'r', encoding='utf-8')
+    line = f.readline()
+    while line:
+        line = f.readline()
+        if line.startswith('server_admin_pw'):
+            print(line)
+            nettool_password_tmp = line
+    f.close()
+    nettool_password = re.sub('^server_admin_pw( = |= | =|=)', '', nettool_password_tmp)
+    print(nettool_password)
+    return nettool_password
+
 def get_pid(process_name):
     for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'] == process_name:
             return proc.info['pid']
     return None
 
+def restart(crash):
+    server_pid = get_pid(config.server_name)
+    print(f'PID: {server_pid}')
+    print(f'server_path: {server_path}')
+    if server_pid is None:
+        subprocess.Popen(['start', server_path, '-server', config.port_number, '-fps', '30'], shell=True)
+    return None
+
+
 # PIDがNoneなら起動する
-server_pid = get_pid(config.server_name)
-print(f'PID: {server_pid}')
-print(f'server_path: {server_path}')
-if server_pid is None:
-    subprocess.Popen(['start', server_path, '-server', config.port_number, '-fps', '30'], shell=True)

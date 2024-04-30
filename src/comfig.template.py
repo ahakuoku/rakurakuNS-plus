f729e2b8d8d5d4654b07b9d61dbe42e030735c85
ahakuoku <ahakuoku@gmail.com>
2024-05-01 00:01:03 +0900

    追加；各種コード

--- src/comfig.template.py
+++ src/comfig.template.py
@@ -6,19 +6,25 @@
 # この設定ファイルは将来的に廃止され、別の手段による設定に置換される予定です。
 # 各設定項目を削除したり、設定値を削除した状態で起動すると思わぬ動作の原因になることがあります。
 # 
+# Discordへの書き込みについて
+# 現在暫定的にSimutrans World Monitorとの連携を使用しています。
+# このため、Discord関連の設定方法は下記の「設定方法」と異なります。
+# 「use_discord_bot」は1の場合はプレーンテキストを書き込み、2の場合はembedで書き込みます。
+# 「discord_token」「discord_channel」は現在使用していません。
+# 
 # 設定方法
-# server_folder_path: サーバーフォルダのパスを指定します。「'」で囲む必要があります。
+# server_folder_path: サーバーフォルダのパスを指定します。「'」で囲む必要があります。フォルダの区切りは「\」ではなく「/」を指定してください。
 # server_name       : サーバーのファイル名を指定します。「'」で囲む必要があります。
 # port_number       : ポート番号を指定します。「'」で囲む必要があります。
 # autosave_backup   : オートセーブのバックアップ数を指定します。
 # autosave_interval : オートセーブの間隔を秒で指定します。間隔は多少ずれるかもしれません。
 # restart_time      : 毎日自動再起動を行うタイミングを時単位で指定します。5時に再起動するのであれば「5」と指定します。-1を指定すると自動再起動を行いません。
-# use_discord_bot   : Discordのbotを使用するかどうか指定します。1もしくは2の場合に使用します。
+# use_discord_bot   : Discordのbotを使用するかどうか指定します。0の場合は使用せず、1もしくは2の場合に使用します。
 # discord_token     : Discordのbotのトークンを指定します。
 # discord_channel   : Discordのbotが書き込むチャンネルを指定します。
 ##################################################
 
-server_folder_path = 'C:/Users/XXX/Simutrans/'
+server_folder_path = 'C:/Users/XXX/Simutrans'
 server_name        = 'sim-WinGDI64-OTRP.exe'
 port_number        = '13353'
 autosave_backup    = 80
--- src/main.py
+++ src/main.py
@@ -4,31 +4,42 @@ import subprocess
 import psutil
 import config
 import time
-import discord
+# import discord
 import re
 
 # 変数定義
-server_path = config.server_folder_path + '/' + config.server_name
+if config.server_folder_path.endswith('/') is True:
+    server_folder_path = config.server_folder_path[:-1]
+else:
+    server_folder_path = config.server_folder_path
+server_path = server_folder_path + '/' + config.server_name
 server_save = '' + config.port_number
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
+# intents = discord.Intents.default()
+# intents.message_content = True
+# client = discord.Client(intents=intents)
 
 # 関数定義
+def check_nettool():
+    try:
+        subprocess.run(['nettool'])
+    except FileNotFoundError:
+        keywait = input(f'nettoolの認識に失敗しました。nettoolを実行ファイルと同じフォルダに置いてからやり直してください。\n（らくらくNS+を終了します。Enterキーを押してください。）')
+    except subprocess.CalledProcessError:
+        keywait = input(f'nettoolの認識に失敗しました。nettoolを実行ファイルと同じフォルダに置いてからやり直してください。\n（らくらくNS+を終了します。Enterキーを押してください。）')
+    return None
+
 def get_nettool_pw():
     # simuconf.tabを開き、「server_admin_pw」から始まる行を検索
-    simuconf_path = config.server_folder_path + '/config/simuconf.tab'
+    simuconf_path = server_folder_path + '/config/simuconf.tab'
     f = open(simuconf_path, 'r', encoding='utf-8')
     line = f.readline()
     while line:
         line = f.readline()
         if line.startswith('server_admin_pw'):
-            print(line)
             nettool_password_tmp = line
     f.close()
     # 行頭の「server_admin_pw = 」を削除し返す
     nettool_password = re.sub('^server_admin_pw( *= *)', '', nettool_password_tmp)
-    print(nettool_password)
     return nettool_password
 
 def get_pid(process_name):
@@ -41,19 +52,34 @@ def get_pid(process_name):
 def restart(crash):
     # pidを取得する
     server_pid = get_pid(config.server_name)
-    # print(f'PID: {server_pid}')
-    # print(f'server_path: {server_path}')
-    # 鯖が落ちてたら再起動
+    # PIDがNoneなら起動する
     if server_pid is None:
         subprocess.Popen(['start', server_path, '-server', config.port_number, '-fps', '30'], shell=True)
-        if config.use_discord_bot != 0:
-            @client.event
-            async def on_ready():
-                # Discordに鯖落ち通知を送信
-                channel = client.get_channel(config.discord_channel)
-                await channel.send(embed=discord.Embed(title='サーバーダウンを検出しました。', description='現在復旧中です。しばらくお待ちください。', color=0xff0000))
-                return None
+        swm_discord_post('サーバーダウンを検出しました。', '現在復旧中です。しばらくお待ちください。', '16711680')
+        # @client.event
+        # async def on_ready():
+            # Discordに鯖落ち通知を送信
+            # channel = client.get_channel(config.discord_channel)
+            # await channel.send(embed=discord.Embed(title='サーバーダウンを検出しました。', description='現在復旧中です。しばらくお待ちください。', color=0xff0000))
+            # return None
+    return None
+
+def swm_discord_post(title, description, color):
+    # Simutrans World Monitorを利用してDiscordに書き込み
+    discord_io_file_plain = server_folder_path + '/file_io/out.txt'
+    discord_io_file_embed = server_folder_path + '/file_io/out_embed.json'
+    if config.use_discord_bot == 1:
+        f = open(discord_io_file_plain, 'w')
+        f.write('# ' + title + '\n' + description + '\n')
+        f.close()
+    elif config.use_discord_bot == 2:
+        title = title.replace('\n', '\\n')
+        description = description.replace('\n', '\\n')
+        f = open(discord_io_file_embed, 'w')
+        f.write('{"description":"' + description + '","fields":null,"title":"' + title + '","color":' + color + ',"footer":null}')
 
 def start():
-    client.run(config.discord_token)
-# PIDがNoneなら起動する
+    restart('test')
+    # client.run(config.discord_token)
+
+start()
